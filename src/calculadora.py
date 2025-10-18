2f7b35b6ee59a7c87b011cf8bf981820bc1f44c8
JoseGongoracCresp <jgongoracrespo@gmail.com>
2025-10-18 19:26:41 +0200

    feat:divisonprint

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,6 +1,13 @@
 def sumar(a,b):
+    print(a)
     return a+b
 def restar (a,b):
     return a-b
+<<<<<<< Updated upstream
 def multiplicar(a,b);
     return a*b
+=======
+def divison(a,b):
+    return a/b              
+
+>>>>>>> Stashed changes
